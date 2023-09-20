5b70a34e4a0c65dbab878dfcc38e93bc0fcb38be
Louis70 <chinedunzeogu@gmail.com>
2023-09-20 10:05:19 +0000

    Alx Interview

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
 '''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-
     '''determines if all the boxes can be opened or not
     Returns:
         True: all boxes can be opened
         False: not all boxes can be opened
     '''
-
     length = len(boxes)
     keys = set()
     opened_boxes = []
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-ALX PROJECTS
+# alx-interview
